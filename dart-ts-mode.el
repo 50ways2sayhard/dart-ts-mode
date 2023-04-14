e9fce186d2023ccda0f1867cd5d66d2f0d9b2625
johngong <johngong@futunn.com>
2023-04-14 18:01:42 +0800

    fix: font-lock for `initialized_variable_definition`

--- dart-ts-mode.el
+++ dart-ts-mode.el
@@ -269,7 +269,7 @@ Argument LANGUAGE is `dart'."
      (initialized_identifier
       (identifier) @font-lock-variable-name-face)
      (initialized_variable_definition
-      (identifier) @font-lock-variable-name-face)
+      name: (identifier) @font-lock-variable-name-face)
      (static_final_declaration
       (identifier) @font-lock-variable-name-face))
 
