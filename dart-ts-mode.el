72dbb4abe3f48b44d299fec37f8aabb3bda78546
dalu <mou.tong@outlook.com>
2023-05-24 04:35:47 +0800

    Fix switch indent behavior
    After tree-sitter-dart's this [commit], `switch_label` has been replaced
    by `switch_statement_case` and `switch_statement_default`.
    
    Besides, change function name and doc to its actual functionality.
    
    [commit]: https://github.com/UserNobody14/tree-sitter-dart/commit/67eef86fa7912ca4b3faae81d5a4b3a2a8b1f3cf

--- dart-ts-mode.el
+++ dart-ts-mode.el
@@ -87,16 +87,16 @@
 
      (no-node parent-bol 0))))
 
-(defun dart-ts-mode--node-bol (node)
-  "Return begining of line of NODE."
+(defun dart-ts-mode--node-start (node)
+  "Return the NODE's start position."
   (save-excursion
     (goto-char (treesit-node-start node))
     (back-to-indentation)
     (point)))
 
-(defun dart-ts-mode--parent-bol (node)
-  "Return parent's begining of line of NODE."
-  (dart-ts-mode--node-bol (treesit-node-parent node)))
+(defun dart-ts-mode--parent-start (node)
+  "Return the position of the first character of NODE's parent."
+  (dart-ts-mode--node-start (treesit-node-parent node)))
 
 (defun dart-ts-mode--if-statement-indent-rule (_ parent &rest __)
   "Indent rule for if_statement.
@@ -105,16 +105,21 @@ returns parent-bol of grandparent.Otherwise returns bol of grandparent."
   (let ((gp (treesit-node-parent parent)))
     (if (and (treesit-node-p gp)
              (string= "if_statement" (treesit-node-string gp)))
-        (dart-ts-mode--parent-bol gp)
-      (dart-ts-mode--node-bol gp))))
+        (dart-ts-mode--parent-start gp)
+      (dart-ts-mode--node-start gp))))
 
 (defun dart-ts-mode--switch-case-indent-rule (node parent &rest __)
   "Indent rule for a NODE under switch_block.
-If NODE is switch_label, returns PARENT's bol.
-Otherwise returns PARENT's bol plus `dart-ts-mode-indent-offset'."
-  (if (string= "switch_label" (treesit-node-type node))
-      (dart-ts-mode--node-bol parent)
-    (+ (dart-ts-mode--node-bol parent) dart-ts-mode-indent-offset)))
+If NODE is switch's label, returns PARENT's start position.
+Otherwise returns PARENT's start position plus
+`dart-ts-mode-indent-offset'."
+  (let ((parent-start (dart-ts-mode--node-start parent))
+        (node-name (treesit-node-type node)))
+    (if (or (string= "switch_label" node-name)
+            (string= "switch_statement_case" node-name)
+            (string= "switch_statement_default" node-name))
+        parent-start
+      (+ parent-start dart-ts-mode-indent-offset))))
 
 (defun dart-ts-mode--arguments-indent-rule (node parent &rest _)
   "Return indentation of argument list.
@@ -123,7 +128,7 @@ starting point of first sibling."
   (let ((first-sibling (treesit-node-child parent 0 t)))
     (if (and first-sibling (not (treesit-node-eq first-sibling node)))
         (treesit-node-start first-sibling)
-      (+ (dart-ts-mode--node-bol parent) dart-ts-mode-indent-offset))))
+      (+ (dart-ts-mode--node-start parent) dart-ts-mode-indent-offset))))
 
 (defun dart-ts-mode--function-body-indent-rule (_ parent &rest _)
   "Return indentation of function_body.PARENT is always a block node."
@@ -131,8 +136,8 @@ starting point of first sibling."
     (if (and maybe-signature
              (or (string-match-p "function_signature" (treesit-node-string maybe-signature))
                  (string-match-p "method_signature" (treesit-node-string maybe-signature))))
-        (dart-ts-mode--node-bol maybe-signature)
-      (dart-ts-mode--node-bol parent))))
+        (dart-ts-mode--node-start maybe-signature)
+      (dart-ts-mode--node-start parent))))
 
 (defun dart-ts-mode--optional-formal-parameters-indent-rule (_ parent &rest __)
   "Return indentation of children of optional_formal_parameters.
@@ -141,7 +146,7 @@ PARENT is always optional_formal_parameters."
     (if (and (treesit-node-p parent-sibling)
              (string= (treesit-node-string parent-sibling) "formal_parameter"))
         (treesit-node-start parent-sibling)
-      (+ (dart-ts-mode--parent-bol parent) dart-ts-mode-indent-offset))))
+      (+ (dart-ts-mode--parent-start parent) dart-ts-mode-indent-offset))))
 
 (defvar dart-ts-mode--keywords
   '("async" "async*" "yield" "sync*"
